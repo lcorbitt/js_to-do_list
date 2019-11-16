5d25a422d4e6a7086f0d1158c833046384aa3776
Lukas Corbitt <movalley5@gmail.com>
2019-11-16 11:46:29 -0700

    Refactored javascript

--- list.js
+++ list.js
@@ -4,11 +4,32 @@ var input = prompt("What would you like to do?")
 
 while(input !== "quit") {
     if(input === "list") {
-        console.log(todos);
+        listToDos();
     } else if(input === "new") {
-    var newToDo = prompt("Enter new to-do");
-        todos.push(newToDo);
-    };
+        addToDo();
+    } else if(input === "delete"){
+        deleteToDo();
+    }
     input = prompt("What would you like to do?");
 }
 console.log("You quit the app.");
+
+function listToDos() {
+    console.log("----------");
+    todos.forEach(function(todo, i){
+        console.log((i) + ": " + todo);
+    });
+    console.log("----------");
+}
+
+function addToDo() {
+    var newToDo = prompt("Enter new to-do");
+    todos.push(newToDo);
+    console.log("Added to-do");
+}
+
+function deleteToDo() {
+    var index = prompt("Enter index of to-do to delete");
+    todos.splice(index, 1);
+    console.log("Deleted to-do");
+}
